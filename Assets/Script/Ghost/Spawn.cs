e7aee4a6d1f61d359e3ea3fcc1515b697d48e1ef
Diana Carolina <dicaro2001@hotmail.com>
2024-02-29 17:30:02 -0500

    Escenario

--- Assets/Script/Ghost/Spawn.cs
+++ Assets/Script/Ghost/Spawn.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     public GameObject spawn;
+    public float spawnTimer;
+    public float spawnMax = 3;
+    public float spawnMin = 1;
     void Start()
     {
-       
-        
+        spawnTimer = Random.Range(spawnMin, spawnMax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(spawn);
+        spawnTimer -= Time.deltaTime;
+        if(spawnTimer <= 0)
+        {
+            Instantiate(spawn);
+            spawnTimer = Random.Range(spawnMin, spawnMax);
+        }
+        
     }
 }
