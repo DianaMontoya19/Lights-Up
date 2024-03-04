58fc7c30052a9777d46348ada7d4dd39c843cfdf
Natsan12 <153123678+Natsan12@users.noreply.github.com>
2024-03-04 15:16:51 -0500

    04/03/2024
    scena cueva, obstaculos

--- Assets/Script/Player/Jump.cs
+++ Assets/Script/Player/Jump.cs
@@ -7,7 +7,7 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody2D rb;
     public Animator anim;
-    public string Vertical;
+   // public string Vertical;
     public bool jump;
     public LayerMask Ground;
     public Transform tf;
@@ -21,11 +21,11 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        float Ver = Input.GetAxisRaw(Vertical);
+        //float Ver = Input.GetAxisRaw(Vertical);
 
         jump = Physics2D.Raycast(tf.position, Vector2.down, 4f, Ground);
 
-        if (Ver != 0 && jump)
+        if (Input.GetKey(KeyCode.UpArrow) && jump)
         {
 
             rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
